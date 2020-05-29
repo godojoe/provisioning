450ddc2629a69ed13fb0e3fdca441d7c8380c779
godojoe <gj@example.com>
2020-05-30 01:01:10 +0200

    vnc

--- ubunu-droplet.sh
+++ ubunu-droplet.sh
@@ -1,10 +1,6 @@
 sudo apt-get update && sudo apt-get dist-upgrade -y
 sudo apt-get install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop -y
 sudo apt-get install mate-core mate-desktop-environment mate-notification-daemon xrdp -y
-adduser remote
-usermod -aG admin remote
-usermod -aG sudo remote
-su - remote
 echo mate-session> ~/.xsession
-sudo cp /home/remote/.xsession /etc/skel
+sudo cp ~/.xsession /etc/skel
 sudo service xrdp restart
